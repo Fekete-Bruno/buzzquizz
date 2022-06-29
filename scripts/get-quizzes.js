5d26a637e279284adf58c996338f4658e23c52e9
Jonatas <jonatasalmeidasm@gmail.com>
2022-06-29 10:50:23 -0300

    correção identação JS

--- scripts/get-quizzes.js
+++ scripts/get-quizzes.js
@@ -2,15 +2,15 @@ const quizzList = [];
 const userQuizzList = [];
 const userId = 8184; /* Apenas para testar */
 
-function getQuizzes(){
+function getQuizzes() {
     const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
     promise.catch(errorLog);
     promise.then(listQuizzes);
 }
 
-function listQuizzes(answer){
+function listQuizzes(answer) {
     answer.data.forEach(quiz => {
-        if (quiz.id === userId){
+        if (quiz.id === userId) {
             userQuizzList.push(quiz);
         } else {
             quizzList.push(quiz);
@@ -19,57 +19,62 @@ function listQuizzes(answer){
     showQuizzes();
 }
 
-function showQuizzes(){
+function showQuizzes() {
     /* inicia a parte de conteudi principal */
     let content = document.querySelector('.content');
-    content.innerHTML='<div class="main-content"></div>';
+    content.innerHTML = '<div class="main-content"></div>';
 
     /* colocar quizzes do usuario */
     let mainContent = document.querySelector('.main-content');
-    if (userQuizzList.length === 0){
-        mainContent.innerHTML +=    `
-                                <div class="create-quizz">
-                                    <p>Você não criou nenhum<br/>quizz ainda :(</p>
-                                    <div onclick="createQuizz();">Criar Quizz</div>
-                                </div> 
-                                `
+    if (userQuizzList.length === 0) {
+        mainContent.innerHTML += 
+            `
+            <div class="create-quizz">
+            <p>Você não criou nenhum<br/>quizz ainda :(</p>
+            <div onclick="createQuizz();">Criar Quizz</div>
+            </div> 
+            `;
     } else {
-            
-        mainContent.innerHTML +=    `
-                                <div class="my-quizzes">SEUS QUIZZES
-                                    <ion-icon name="add-circle"></ion-icon>
-                                </div>
-                                <div class="user-quizz-container"></div>
-                                `;
+
+        mainContent.innerHTML += 
+            `
+            <div class="my-quizzes">SEUS QUIZZES
+            <ion-icon name="add-circle"></ion-icon>
+            </div>
+            <div class="user-quizz-container"></div>
+            `;
         let userQuizzContainer = document.querySelector('.user-quizz-container');
         userQuizzList.forEach(quiz => {
-            userQuizzContainer.innerHTML += `
-                                            <div class="quizz">
-                                                <img src="${quiz.image}" alt="">
-                                                <h1>${quiz.title}</h1>
-                                            </div>
-                                            `            
+            userQuizzContainer.innerHTML += 
+            `
+            <div class="quizz">
+            <img src="${quiz.image}" alt="">
+            <h1>${quiz.title}</h1>
+            </div>
+            `
         });
 
     }
 
 
-    mainContent.innerHTML +=  `
-                    <div class="all-quizzes">TODOS OS QUIZZES</div>
-                    <div class="quizz-container"></div>
-                    `;
+    mainContent.innerHTML += 
+        `
+        <div class="all-quizzes">TODOS OS QUIZZES</div>
+        <div class="quizz-container"></div>
+        `;
     let quizzContainer = document.querySelector('.quizz-container');
     quizzList.forEach(quiz => {
-        quizzContainer.innerHTML += `
-                                        <div class="quizz">
-                                            <img src="${quiz.image}" alt="">
-                                            <h1>${quiz.title}</h1>
-                                        </div>
-                                        `            
+        quizzContainer.innerHTML += 
+            `
+            <div class="quizz">
+            <img src="${quiz.image}" alt="">
+            <h1>${quiz.title}</h1>
+            </div>
+            `
     });
 }
 
-function errorLog(error){
+function errorLog(error) {
     console.log(error.data);
 }
 
