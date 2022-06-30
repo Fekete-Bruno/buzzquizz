7c6e7295ac634c883473de3766ca0a9ab45ddfbb
Fekete-Bruno <brunohffekete@gmail.com>
2022-06-29 21:53:14 -0300

    js - tela verifica os inputs e mostra o alert

--- scripts/create-quizzes.js
+++ scripts/create-quizzes.js
@@ -1,3 +1,5 @@
+    let inputs;
+    let obj;
     function createQuizz(){
         console.log('Criando Quiz...');
         loadQuizzCreation();
@@ -9,15 +11,47 @@
                 <h2>Começe pelo começo</h2>
 
                 <div class="quizz-options">
-                <div><input type="text" placeholder="Título do seu quiz"></div>
-                <div><input type="text" placeholder="URL da imagem do seu quiz"></div>
-                <div><input type="text" placeholder="Quantidade de perguntas do quizz"></div>
-                <div><input type="text" placeholder="Quantidade de níveis do quizz"></div>
+                <div><input type="text" placeholder="Título do seu quiz" ></div>
+                <div><input type="text" placeholder="URL da imagem do seu quiz" ></div>
+                <div><input type="text" placeholder="Quantidade de perguntas do quizz" ></div>
+                <div><input type="text" placeholder="Quantidade de níveis do quizz" ></div>
                 </div>
 
-                <button>Prosseguir para criar perguntas</button>
+                <button onclick="checkStart()">Prosseguir para criar perguntas</button>
             </div>
             `; 
+    }
 
+    function checkStart(){
+        inputs = document.querySelectorAll('input');
+        obj = {
+            title: inputs[0].value,
+            URL : inputs[1].value,
+            questionAmount: inputs[2].value,
+            levelsAmount: inputs[3].value
+        }
 
+        if(obj.title.length<20 || obj.title.length>65 || !isUrlValid(obj.URL) || obj.questionAmount<3 || obj.levelsAmount<2){
+            alert('Verifique as suas configurações de quiz novamente...');
+        } else {
+            /* Aqui deve ativar a próxima tela */
+            console.log('true');
+        }
+
+    }
+
+    /*
+    function questionLog(){
+
+    }
+    */
+   
+    function isUrlValid(userInput) {
+        let res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+        if(res == null){    
+            return false;
+        }
+        else{
+            return true;
+        }
     }
