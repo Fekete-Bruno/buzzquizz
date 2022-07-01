7aa6358b1e6ecde517e28d1c372ca59b36268f2a
Jonatas <jonatasalmeidasm@gmail.com>
2022-07-01 17:09:37 -0300

    page 3.3 start

--- scripts/create-quizzes.js
+++ scripts/create-quizzes.js
@@ -34,8 +34,7 @@
         if(obj.title.length<20 || obj.title.length>65 || !isUrlValid(obj.URL) || obj.questionAmount<3 || obj.levelsAmount<2){
             alert('Verifique as suas configurações de quiz novamente...');
         } else {
-            /* Aqui deve ativar a próxima tela */
-            console.log('true');
+            loadquestionLevels();
         }
 
     }
@@ -45,6 +44,27 @@
 
     }
     */
+
+    function loadquestionLevels(){
+        document.querySelector('.content').innerHTML=
+            `
+            <div class="quizz-levels">
+                <h2>Agora, decida os níveis!</h2>
+
+                <div class="level-options">
+                    <div class="level1"> Nível 1
+                        <div><input type="text" placeholder="Título do nível" ></div>
+                        <div><input type="text" placeholder="% de acerto mínima" ></div>
+                        <div><input type="text" placeholder="URL da imagem do nível" ></div>
+                        <div><textarea rows="5" placeholder="Descrição do nível"></textarea></div>
+                    </div>    
+                </div>
+
+                <button onclick="">FinalizarQuizz</button>
+            </div>
+            `; 
+    }
+
    
     function isUrlValid(userInput) {
         let res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
