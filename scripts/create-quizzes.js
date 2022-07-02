5807a3cd5b3b94656364865cabd31060086b633f
Fekete-Bruno <brunohffekete@gmail.com>
2022-07-01 21:05:59 -0300

    start of questions

--- scripts/create-quizzes.js
+++ scripts/create-quizzes.js
@@ -34,15 +34,39 @@
         if(quizzInfo.title.length<20 || quizzInfo.title.length>65 || !isUrlValid(quizzInfo.URL) || quizzInfo.questionAmount<3 || quizzInfo.levelsAmount<2){
             alert('Verifique as suas configurações de quiz novamente...');
         } else {
-            loadquestionLevels();
+            loadQuestions();
         }
 
     }
 
     
-    /*function loadQuestions(){
+    function loadQuestions(){
+        document.querySelector('.content').innerHTML = 
+        `
+        <div class="quizz-questions"> 
+            <h2>Agora, decida os níveis!</h2>
+        </div>
+        `;
 
-    }*/
+        for (let i = 1; i<=quizzInfo.questionAmount; i++){
+            document.querySelector('.quizz-questions').innerHTML +=
+            `
+                <div class="question-options">
+                    <div class="question${i}"> Pergunta ${i}
+                        <div><input type="text" placeholder="Texto da pergunta" ></div>
+                        <div><input type="text" placeholder="Cor de fundo da pergunta" ></div>
+                        <div><input type="text" placeholder="URL da imagem do nível" ></div>
+                        <div><input type="text" placeholder="Descrição do nível"></textarea></div>
+                    </div>    
+                </div>
+            `; 
+        }
+
+        document.querySelector('.quizz-questions').innerHTML += 
+        `
+            <button onclick="loadquestionLevels();">Continuar...</button>
+        `
+    }
     
 
     function loadquestionLevels(){
