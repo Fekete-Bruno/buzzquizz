d615d2c42d5b95913f1949afd26c1f66035b56c0
Fekete-Bruno <brunohffekete@gmail.com>
2022-07-03 20:27:04 -0300

    tela de sucesso funcionando

--- scripts/create-quizzes-post.js
+++ scripts/create-quizzes-post.js
@@ -13,6 +13,24 @@ function preparePost(){
 }
 
 function postSuccess(answer) {
+    successScreen();
     console.log('alright');
     console.log(answer);
+}
+
+function successScreen(){
+    let content = document.querySelector('.content');
+    content.innerHTML = `
+                        <div class="success-screen">
+                        <h2>Seu quizz está pronto!</h2>
+                        <div class="quizz">
+                            <img src="./assets/simpsons.jpg" alt="">
+                            <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
+                        </div>
+                        <div>
+                            <button class="restart-quizz" onclick="">Acessar Quizz</button>
+                            <button class="home" onclick="returnHome()">Voltar para home</button>
+                        </div>
+                        </div>
+                        `;
 }
